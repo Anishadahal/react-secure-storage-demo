354a544710ba8a09d8e125ff2957109148f3115b
Anisha Dahal <anisha.dahal@webo.digital>
2023-01-25 00:31:16 +0545

    feat: react secure storage implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,91 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from "react";
+import secureLocalStorage from "react-secure-storage";
 
-const Home: NextPage = () => {
+const Home = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");  
+  const [fetchedUsername, setFetchedUsername] = useState("");
+  const [fetchedPassword, setFetchedPassword] = useState("");
+
+  const handleSubmit = () => {
+    // Encrypt credentials
+    secureLocalStorage.setItem("username", username);
+    secureLocalStorage.setItem("password", password);
+  };
+
+  const handleRetrieve = () => {
+    // Retrieve and decrypt credentials
+    const decryptedUsername = secureLocalStorage.getItem("username") as string;
+    const decryptedPassword = secureLocalStorage.getItem("password") as string;
+    setFetchedUsername(decryptedUsername);
+    setFetchedPassword(decryptedPassword);
+  };
+
+  const handleRemovePassword = () => {
+    // Remove credentials
+    secureLocalStorage.removeItem("password");
+    handleRetrieve();
+  };
+  const handleClear = () => {
+    // Clear credentials
+    secureLocalStorage.clear();
+    handleRetrieve();
+  };
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex flex-col items-center mt-24">
+      <div className="flex flex-col gap-1 mb-4 text-sm">
+        <label htmlFor="">Username</label>
+        <input
+          className="p-2 border rounded"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+      </div>
+      <div className="flex flex-col gap-1 mb-4 text-sm">
+        <label htmlFor="">Password</label>
+        <input
+          className="p-2 border rounded"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+      </div>
+      <div className="flex justify-around gap-2">
+        <button
+          className="p-2 text-white bg-blue-500 rounded hover:shadow-md hover:bg-blue-600"
+          onClick={handleSubmit}
         >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+          Submit
+        </button>
+        <button
+          className="p-2 text-white bg-blue-500 rounded hover:shadow-md hover:bg-blue-600"
+          onClick={handleRetrieve}
+        >
+          Retrieve
+        </button>
+        <button
+          className="p-2 text-white bg-blue-500 rounded hover:shadow-md hover:bg-blue-600"
+          onClick={handleRemovePassword}
+        >
+          Remove password
+        </button>
+        <button
+          className="p-2 text-white bg-blue-500 rounded hover:shadow-md hover:bg-blue-600"
+          onClick={handleClear}
+        >
+          Clear
+        </button>
+      </div>
+      <div className="flex flex-col items-center mt-4">
+        <p className="text-xl">Username: {fetchedUsername}</p>
+        <p className="text-xl">Password: {fetchedPassword}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
